a01ca2788a5f3ada6b460a943b0d28159e355e4c
ShengQiangLiu <1365688791@qq.com>
2016-12-23 19:07:00 +0800

    Save

--- 线程与锁/线程与锁/ViewController.m
+++ 线程与锁/线程与锁/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import <pthread.h>
 #import "BigDog.h"
+#import "libkern/OSAtomic.h"
 
 @interface ViewController ()
 
@@ -80,36 +81,36 @@
     /**
      使用POSIX（条件锁）创建锁
      */
-    // 创建互斥锁
-    __block pthread_mutex_t  mutex;
-    pthread_mutex_init(&mutex, NULL);
-    
-    // 创建条件锁
-    __block pthread_cond_t cond;
-    pthread_cond_init(&cond, NULL);
-    
-    // 线程 A
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        pthread_mutex_lock(&mutex);
-        pthread_cond_wait(&cond, &mutex);
-        
-        NSLog(@"线程 A3 。");
-
-        
-        pthread_mutex_unlock(&mutex);
-    });
-
-
-    // 线程 B
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        pthread_mutex_lock(&mutex);
-        
-        NSLog(@"线程 B3 。");
-        [NSThread sleepForTimeInterval:3];
-
-        pthread_cond_signal(&cond);
-        pthread_mutex_unlock(&mutex);
-    });
+//    // 创建互斥锁
+//    __block pthread_mutex_t  mutex;
+//    pthread_mutex_init(&mutex, NULL);
+//    
+//    // 创建条件锁
+//    __block pthread_cond_t cond;
+//    pthread_cond_init(&cond, NULL);
+//    
+//    // 线程 A
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        pthread_mutex_lock(&mutex);
+//        pthread_cond_wait(&cond, &mutex);
+//        
+//        NSLog(@"线程 A3 。");
+//
+//        
+//        pthread_mutex_unlock(&mutex);
+//    });
+//
+//
+//    // 线程 B
+//    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+//        pthread_mutex_lock(&mutex);
+//        
+//        NSLog(@"线程 B3 。");
+//        [NSThread sleepForTimeInterval:3];
+//
+//        pthread_cond_signal(&cond);
+//        pthread_mutex_unlock(&mutex);
+//    });
     /**
      效果：程序会首先调用线程B，在5秒后再调用线程A。因为在线程A中创建了等待条件锁，线程B有激活锁，只有当线程B执行完后会激活线程A。
      
@@ -118,6 +119,31 @@
      pthread_cond_signal方法为激动一个相同条件的条件锁。
      */
 
+
+    /**
+     使用 OSSpinLock 创建锁
+     */
+    __block OSSpinLock spinlock = OS_SPINLOCK_INIT;
+    // 线程 A
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        OSSpinLockLock(&spinlock);
+        
+        NSLog(@"线程 A4 。");
+        [NSThread sleepForTimeInterval:3];
+
+        OSSpinLockUnlock(&spinlock);
+    });
+
+
+    // 线程 B
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        OSSpinLockLock(&spinlock);
+        
+        NSLog(@"线程 B4 。");
+        [NSThread sleepForTimeInterval:3];
+
+        OSSpinLockUnlock(&spinlock);
+    });
     
 }
 
